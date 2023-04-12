ab5a3c9c077bc18400815bbbbf3d90d9770c61b3
Rosa Jiménez <rosa3jm@gmail.com>
2023-04-12 20:28:36 +0200

    404 xd

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<script>
+import './../style.css'
+</script>
+<template>
+
+
+</template>
+
+<style>
+body{
+  background-image: url("../../public/404.gif");
+  background-repeat:no-repeat;
+  background-position:center;
+  background-size: cover;
+}
+</style>
